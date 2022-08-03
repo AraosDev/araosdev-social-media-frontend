2f3f99063e9b938fb98611973a3b582295a9a29e
SeenuAravindRaj <84611330+Mudivira-Digital-Solutions@users.noreply.github.com>
2022-08-04 00:23:01 +0530

    Timeline read only images commit

--- src/Common/helperFns.js
+++ src/Common/helperFns.js
@@ -1,5 +1,5 @@
 export const unixTimeToReadableFormat = (unixTime) => {
-    const date = new Date(unixTime * 1000);
+    const date = new Date(unixTime*1000);
     const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
     let formattedDate = `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`;
 
--- src/Pages/TimeLine/TimelineBody.js
+++ src/Pages/TimeLine/TimelineBody.js
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components'
 import { Loader } from '../../Common/DataTransitionHandlers';
+import Card from 'react-bootstrap/Card';
 import { getTimeLineImagesAction } from '../../Store/actions/timelineActions';
+import ProfileIcon from '../../Common/ProfileIcon';
+import { BsHeart, BsChat } from "react-icons/bs";
 
 const StyledTimelineBody = styled.div`
     max-height: calc(100vh - 85px);
@@ -21,15 +24,50 @@ const StyledTimelineBody = styled.div`
 
 function TimelineBody() {
     const dispatch = useDispatch();
+    const { timelineState, timelineImages } = useSelector(state => state.timelineReducer);
 
     useEffect(() => {
         dispatch(getTimeLineImagesAction());
-    }, [])
+    }, []);
+
+    const getTimelineContent = () => {
+        switch(timelineState) {
+            case 'TIMELINE_LOADING': return (
+                <Loader className='loader-element caveatBold' message='Loading your feeds . . .' />
+            );
+
+            case 'TIMELINE_LOADED': return(
+                <div className='py-3'>
+                    {
+                        timelineImages.map((image, index) => (
+                            <Card className={`${index === (timelineImages.length - 1) ? '' : 'mb-4'}`}>
+                                <Card.Header className='d-flex'>
+                                    <ProfileIcon iconText={image.userName.charAt(0).toUpperCase()} />
+                                    <div className='d-flex flex-column ms-2' style={{ fontSize: 13, fontWeight: 700 }}>
+                                        <span>{image.userName}</span>
+                                        <span>{image.postedDate}</span>
+                                    </div>
+                                </Card.Header>
+                                <Card.Body>
+                                    <Card.Subtitle className= 'mb-3'>{image.caption}</Card.Subtitle>
+                                    <Card.Img style={{ width: '100%', height: 600, objectFit: 'cover' }} src={image.imageLink} />
+                                </Card.Body>
+                                <Card.Footer>
+                                    <BsHeart size={25} className='me-2' />{image.likes} likes
+                                    <BsChat size={24} className='mx-2' />{image.commentSection.length} comments
+                                </Card.Footer>
+                            </Card>
+                        ))
+                    }
+                </div>
+            );
+        }
+    }
     
     return (
         <StyledTimelineBody>
             <Container className='timeline-body-container'>
-                <Loader className='loader-element caveatBold' message='Loading your feeds . . .' />
+                {getTimelineContent()}
             </Container>
         </StyledTimelineBody>
     )
