ec1b11f745737293a744bb6658b494a437f3c7f1
AraosDev <88236041+AraosDev@users.noreply.github.com>
2023-05-24 16:39:50 +0530

    Rerouting to different device

--- server/index.js
+++ server/index.js
@@ -4,10 +4,15 @@ const express = require('express');
 const app = express();
 
 const apiProxy = createProxyMiddleware({
-  target: 'http://localhost:5000/api/v1/adsm/',
+  router: (req) => {
+    console.log(req.originalUrl);
+    if (req.originalUrl.includes('AUTHNZ'))
+      return 'http://localhost:5000/api/v1/adsm/';
+    return 'http://localhost:5001/araosdevsm/';
+  },
   changeOrigin: true,
   pathRewrite(path) {
-    return path.replace('AUTHNZ', 'autnN');
+    return path.replace('AUTHNZ', 'autnN').replace('ADSM', '');
   },
 });
 
